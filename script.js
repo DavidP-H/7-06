058f12d4ed61d421d227caf68bff25df035ff116
DavidHart05 <>
2021-05-11 16:31:19 +0000

    spacing

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let number = 15
+const number = 15
 let guess
 
 document.getElementById('button').addEventListener('click', checker)
 
 function checker () {
- while (guess != number) {
-   guess = prompt('Guess an age.')
+  while (guess != number) {
+    guess = prompt('Guess an age.')
     if (guess == number) {
-    alert('You guessed it right!')
-  } else if (guess > number) {
-    alert('You guessed too high, do you want to try again?')
-  } else if (guess < number) {
-    alert('You guessed too low, do you want to try again?')
-  } else {
-    alert('Invalid answer, please try again.')
-  }
- }
+      alert('You guessed it right!')
+    } else if (guess > number) {
+      alert('You guessed too high, do you want to try again?')
+    } else if (guess < number) {
+      alert('You guessed too low, do you want to try again?')
+    } else {
+      alert('Invalid answer, please try again.')
+    }
+   }
 }
